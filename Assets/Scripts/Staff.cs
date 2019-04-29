6f85eccb84035f673e91e518bdc8360cf0fa6eb6
zhshtabbito <zhshtabbito@foxmail.com>
2019-04-30 03:32:53 +0800

    add-staff

---
+++ Assets/Scripts/Staff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Staff : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<CharacterController2D>().MoveSpd(new Vector2(0, 0.8f));
+        StartCoroutine("WaitAndStop");
+    }
+
+    IEnumerator WaitAndStop()
+    {
+        yield return new WaitForSeconds(12);
+        GetComponent<CharacterController2D>().MoveSpd(Vector2.zero);
+        yield return new WaitForSeconds(5);
+        SceneManager.LoadScene(0);
+    }
+
+
+
+}
